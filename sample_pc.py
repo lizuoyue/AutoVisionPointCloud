e352072ca2a1c5a76707f9d30dba20387ea53eec
li.zuoyue@foxmail.com <lizuoyue@lizuoyue.local>
2020-04-16 16:31:39 +0900

    a

--- sample_pc.py
+++ sample_pc.py
@@ -1,9 +1,15 @@
 from utils import get_pc_nxstr
 import random
+import os
 
 if __name__ == '__main__':
 
     random.seed(7)
+    path="data/2018-10-18-Lim-Chu-Kang-Run-1-Day/point_clouds_length_1000m_overlap_100m/point_cloud"
+    for i in range(9):
+        os.popen(f'zip {path}_sample_{i}.zip {path}_{i}.txt')
+    quit()
+
     for i in range(9):
         print(i)
         path = f'data/2018-10-18-Lim-Chu-Kang-Run-1-Day/point_clouds_length_1000m_overlap_100m/point_cloud_{i}.zip'
