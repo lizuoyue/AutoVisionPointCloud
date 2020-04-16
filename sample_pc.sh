f23cf317cb1b1866b08ed06d366b641f079b6136
li.zuoyue@foxmail.com <lizuoyue@lizuoyue.local>
2020-04-16 16:25:08 +0900

    a

---
+++ sample_pc.sh
@@ -0,0 +1,7 @@
+PATH="data/2018-10-18-Lim-Chu-Kang-Run-1-Day/point_clouds_length_1000m_overlap_100m/point_cloud_"
+for (( i=0; i<=8; ++i ))
+do
+    zip "$PATH""sample_$i.zip"" $PATH$i.txt"
+done
+# zip *.zip *.txt
+# rm *.txt
