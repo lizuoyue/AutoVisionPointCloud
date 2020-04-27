ac6e82dd576be69a91c458e88b3e18850c12e010
li.zuoyue@foxmail.com <lizuoyue@lizuoyue.local>
2020-04-27 15:34:45 +0900

    a

--- label_point_cloud.py
+++ label_point_cloud.py
@@ -21,6 +21,7 @@ if __name__ == '__main__':
     WHICH_PC = np.cumsum(WHICH_PC)
     assert(PC_NUM_SEP == (len(SEP) - 1))
     assert((FRAME_TO - FRAME_FROM) == SEP[-1])
+    SHOW_TIME = True
 
     host_name = socket.gethostname()
 
@@ -65,7 +66,7 @@ if __name__ == '__main__':
     for i, pose in tqdm.tqdm(list(enumerate(cam_poses[FRAME_FROM: FRAME_TO]))):
 
         if i in SEP:
-            pc_coord = pc_str_lines2nxXYZ1(get_pc_nxstr(pc_path % WHICH_PC[i]))
+            pc_coord = pc_str_lines2nxXYZ1(get_pc_nxstr(pc_path % WHICH_PC[i], show_time=SHOW_TIME), show_time=SHOW_TIME)
             pc_index = np.arange(pc_coord.shape[0])
 
         if False:
@@ -73,8 +74,12 @@ if __name__ == '__main__':
             depth = depth.reshape((-1))
             depth_valid = (0 < depth) & (depth < MAX_Z)
         else:
+            tic = time.time()
             depth = json.load(open(depth_path.replace('.pgm', '.json') % (i + FRAME_FROM)))
             depth = np.array(depth['depth_map']['data'])
+            toc = time.time()
+            if SHOW_TIME:
+                print('Loading depth file costs %.3lf seconds.' % (toc - tic))
             depth_valid = (0 < depth)
         depth_min = depth * (1 - EPSILON)
         depth_max = depth * (1 + EPSILON)
@@ -83,13 +88,15 @@ if __name__ == '__main__':
         mat_world_to_cam = np.linalg.inv(mat_cam_to_world)
         cam_loc = mat_cam_to_world[:3, 3]
 
+        tic = time.time()
+
         # Filter 1 - only consider the points near the camera center
         idx =       (pc_coord[:, 0] > cam_loc[0] - CUBE)
         idx = idx & (pc_coord[:, 0] < cam_loc[0] + CUBE)
         idx = idx & (pc_coord[:, 1] > cam_loc[1] - CUBE)
         idx = idx & (pc_coord[:, 1] < cam_loc[1] + CUBE)
-        pc_cam_coord = pc_coord[idx].copy()
-        pc_cam_index = pc_index[idx].copy()
+        pc_cam_coord = pc_coord[idx]
+        pc_cam_index = pc_index[idx]
 
         # Rotate to the camera coordinate system
         pc_cam_coord = mat_world_to_cam[:3].dot(pc_cam_coord.T)
@@ -116,11 +123,19 @@ if __name__ == '__main__':
         # Get image 1D index
         img_1d_idx = y * img_size[0] + x
 
+        toc = time.time()
+        if SHOW_TIME:
+            print('Computing part A costs %.3lf seconds.' % (toc - tic))
+
         # Filter 4 - only choose one point for each pixel
+        tic = time.time()
         idx = verify_depth(img_1d_idx, pc_z, depth)
         pc_z = pc_z[idx]
         img_1d_idx = img_1d_idx[idx]
         pc_cam_index = pc_cam_index[idx]
+        toc = time.time()
+        if SHOW_TIME:
+            print('Verifying depth costs %.3lf seconds.' % (toc - tic))
 
         # Filter 5 - only consider a point which has a valid ground truth depth
         idx = depth_valid[img_1d_idx]
@@ -129,6 +144,7 @@ if __name__ == '__main__':
         pc_cam_index = pc_cam_index[idx]
 
         if True:
+            tic = time.time()
             gt_depth = depth
             fake_depth = depth * 0
             fake_depth[img_1d_idx] = pc_z
@@ -138,6 +154,9 @@ if __name__ == '__main__':
             ])
             to_show = (cmap((to_show - to_show.min()) / (to_show.max() - to_show.min())) * 255).astype(np.uint8)
             Image.fromarray(to_show).save('fake_depth/%05d.png' % (i + FRAME_FROM))
+            toc = time.time()
+            if SHOW_TIME:
+                print('Creating fake depth costs %.3lf seconds.' % (toc - tic))
 
         # Filter 6 - only consider a point which has an accurate depth
         idx = (depth_min[img_1d_idx] < pc_z) & (pc_z < depth_max[img_1d_idx])
@@ -146,16 +165,25 @@ if __name__ == '__main__':
         pc_cam_index = pc_cam_index[idx]
 
         # Write log and result
-        f_log.write('%d %.6lf %d' % (i + FRAME_FROM, idx.mean(), idx.sum()))
+        tic = time.time()
+        f_log.write('%d %.6lf %d\n' % (i + FRAME_FROM, idx.mean(), idx.sum()))
+        f_log.flush()
         with open('result/%05d.txt' % (i + FRAME_FROM), 'w') as f_res:
             f_res.write(f'{WHICH_PC[i]}\n')
             for a, b in zip(img_1d_idx, pc_cam_index):
                 f_res.write(f'{a} {b}\n')
+                f_res.flush()
+        toc = time.time()
+        if SHOW_TIME:
+            print('Writing to file costs %.3lf seconds.' % (toc - tic))
 
         if True:
+            tic = time.time()
             fake_img = np.array(Image.open(img_path % (i + FRAME_FROM))).reshape((-1))
             fake_img[img_1d_idx] = 0
             Image.fromarray(fake_img.reshape(img_size[::-1])).save('fake_img/%05d.png' % (i + FRAME_FROM))
+            if SHOW_TIME:
+                print('Creating fake image costs %.3lf seconds.' % (toc - tic))
 
     f_log.close()
 
