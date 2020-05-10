b4ddd873e04abb129d484fe92d2310f210031340
li.zuoyue@foxmail.com <lizuoyue@staff-net-vpn-dhcp-3823.ethz.ch>
2020-05-10 17:49:19 +0900

    a

--- sample_pc_night.py
+++ sample_pc_night.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     mat_path = 'data/2018-11-01-Lim-Chu-Kang-Run-3-Night/point_cloud/icp_T_day_night/point_cloud_%d_T_day_night.txt'
     random.seed(7)
     li = []
-    for i in tqdm.tqdm(list(range(2, 12))):
+    for i in tqdm.tqdm(list(range(2, 102))):
         pc = np.loadtxt(pc_path % i)
         mat = np.loadtxt(mat_path % i)
         num = int(pc.shape[0] / 100)
@@ -17,4 +17,4 @@ if __name__ == '__main__':
         pc[:,3] = 1
         li.append(mat.dot(pc[:num,:4].T).T)
     pc = np.concatenate(li)
-    np.savetxt('merge.txt', pc, fmt='%.12f', delimiter=';')
+    np.savetxt('merge_night_sample.txt', pc, fmt='%.12f', delimiter=';')
