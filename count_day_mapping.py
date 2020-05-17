9ca19ff659b650a18b6ac7cc1b69c6008bb6c980
li.zuoyue@foxmail.com <lizuoyue@lizuoyue.local>
2020-05-17 22:45:14 +0900

    a

--- count_day_mapping.py
+++ count_day_mapping.py
@@ -3,8 +3,8 @@ import glob
 import json
 
 for file in sorted(glob.glob('0_day_mapping/*.npz')):
-	n1 = np.load(file)['img_1d_idx'].shape[0]
-	depth = json.load(open('data/2018-11-01-Lim-Chu-Kang-Run-3-Night/img_depth/DEV_000F3102F884/%s.json' % file[-9: -4]))
+	n1 = np.unique(np.load(file)['img_1d_idx']).shape[0]
+	depth = json.load(open('data/2018-10-18-Lim-Chu-Kang-Run-1-Day/gt_depth/%s.json' % file[-9: -4]))
 	depth = np.array(depth['depth_map']['data'])
 	n2 = (depth > 0).sum()
 	print(int(file[-9: -4]), n1/n2)
