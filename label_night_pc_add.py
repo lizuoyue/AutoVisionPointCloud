8aef5277f02670ca7616735a640acd3f120f4de0
li.zuoyue@foxmail.com <lizuoyue@lizuoyue.local>
2020-05-18 21:16:29 +0900

    a

---
+++ label_night_pc_add.py
@@ -0,0 +1,11 @@
+import numpy as np
+import glob
+import os
+
+files = sorted(glob.glob('2_night_pc_label_add/*.npz'))
+for file in files:
+	a, b = os.path.basename(file).split('_')
+	a = int(a) - 1
+	label = np.load(f'2_night_pc_label/{a}_{b}')
+	new_label = np.load(file)
+	print(file, (new_label[label == 255] < 255).mean())
