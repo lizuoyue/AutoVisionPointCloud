76b02982f07aba52f72a6b87660d6afbff627ec9
li.zuoyue@foxmail.com <lizuoyue@lizuoyue.local>
2020-05-18 21:17:22 +0900

    a

--- label_night_pc_add.py
+++ label_night_pc_add.py
@@ -6,6 +6,6 @@ files = sorted(glob.glob('2_night_pc_label_add/*.npz'))
 for file in files:
 	a, b = os.path.basename(file).split('_')
 	a = int(a) - 1
-	label = np.load(f'2_night_pc_label/{a}_{b}')
-	new_label = np.load(file)
+	label = np.load(f'2_night_pc_label/{a}_{b}')['label']
+	new_label = np.load(file)['label']
 	print(file, (new_label[label == 255] < 255).mean())
