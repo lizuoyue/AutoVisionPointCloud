eb06049b548e688cec1e728b08c5ef6b35a2d908
li.zuoyue@foxmail.com <lizuoyue@lizuoyue.local>
2020-05-20 17:26:49 +0900

    a

---
+++ make_dataset.py
@@ -0,0 +1,35 @@
+import numpy as np
+import glob, tqdm, os
+from PIL import Image
+from utils import *
+
+if __name__ == '__main__':
+
+	colormap = create_autovision_simple_label_colormap()
+
+	img_path = 'data/2018-11-01-Lim-Chu-Kang-Run-3-Night/img_fisheye/DEV_000F3102F884'
+	sem_path = '3_night_sem'
+	sem_path_add = '3_night_sem_add'
+
+	save_path = 'vis'
+	save_alpha_path = 'vis_alpha'
+
+	os.system(f'mkdir {save_path} {save_alpha_path}')
+
+	for i in tqdm.tqdm(list(range(20000))):
+
+		sem_add = f'{sem_path_add}/%05d.png' % i
+		sem = f'{sem_path}/%05d.png' % i
+		img = f'{img_path}/%05d.png' % i
+
+		if os.path.isfile(sem_add):
+			sem = np.array(Image.open(sem_add).convert('L'))
+		elif:
+			sem = np.array(Image.open(sem).convert('L'))
+		else:
+			continue
+		img = np.array(Image.open(img).convert('RGB'))
+		sem = colormap[sem.flatten()].reshape(img.shape[:2])
+
+		Image.fromarray((img * 0.7 + sem * 0.3).astype(np.uint8)).save(f'{save_path}/%05d.png' % i)
+		Image.fromarray(np.vstack([img, sem])).save(f'{save_alpha_path}/%05d.png' % i)
